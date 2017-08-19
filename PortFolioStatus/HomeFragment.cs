52babadf655c418cbd15d67ed56053a5ddad0d5d
Shubham <yesshubham@gmail.com>
2017-08-19 23:18:18 +0530

    Update the app to test net connection and modify the list of stocks

--- PortFolioStatus/HomeFragment.cs
+++ PortFolioStatus/HomeFragment.cs
@@ -1,7 +1,9 @@
-﻿using Android.OS;
+﻿using Android.Net;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
 using System.Collections.Generic;
+using System;
 
 namespace PortFolioStatus
 {
@@ -12,12 +14,42 @@ namespace PortFolioStatus
         {
 
             View view = inflater.Inflate(Resource.Layout.Home, null);
-            stocksList.Add("First");
-            stocksList.Add("Second");
+            for(int i = 0; i  < 200; i++)
+                stocksList.Add("Demo");
             ListView stocksListView = view.FindViewById<ListView>(Resource.Id.stocksList);
             stocksListView.Adapter = new StocksListViewAdapter(this.Activity, stocksList);
             stocksListView.ItemClick += (o, e) => Toast.MakeText(inflater.Context, "You clicked" + e.Position, ToastLength.Long).Show();
+            var btn = view.FindViewById<Button>(Resource.Id.btnSync);
+            var ctx = this.Activity.ApplicationContext;
+            btn.Click += (o, e) => {
+                                        var manager = (ConnectivityManager)ctx.GetSystemService(Android.Content.Context.ConnectivityService);
+                                        var netInfo = manager.ActiveNetworkInfo;
+                                        var isOnline = netInfo.IsConnected;
+                                        if (isOnline)
+                                        {
+                                            Toast.MakeText(ctx, "Connected to network!", ToastLength.Short).Show();
+                                            Toast.MakeText(ctx, "Calling google finance api!", ToastLength.Short).Show();
+                                            var response = GetStocksInfo();
+                                            Toast.MakeText(ctx, "Got Response!", ToastLength.Short).Show();
+                                            stocksList.Clear();
+                                            stocksList.AddRange(response);
+                                            stocksListView.Adapter = new StocksListViewAdapter(this.Activity, stocksList);
+                                            ((StocksListViewAdapter)stocksListView.Adapter).NotifyDataSetChanged();
+                                        }
+                                        else
+                                        {
+                                            Toast.MakeText(ctx, "Not Connected to network!\nPlease connect to network!", ToastLength.Long).Show();
+                                        }
+            };
             return view;
+        }
+
+        private List<string> GetStocksInfo()
+        {
+            var res = new List<string>();
+            for (int i = 0; i < 200; i++)
+                res.Add("new val");
+            return res;
         }
     }
 }
