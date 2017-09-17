3d9f8329d22addfb3b500f00cde62e51efc99b83
Shubham <yesshubham@gmail.com>
2017-09-18 00:55:03 +0530

    create fields in add stock item screen

--- PortFolioStatus/HomeAdd.cs
+++ PortFolioStatus/HomeAdd.cs
@@ -22,7 +22,24 @@ namespace PortFolioStatus
 
             var saveBtn = FindViewById<Button>(Resource.Id.btnHomeSave);
             var cancelBtn = FindViewById<Button>(Resource.Id.btnHomeCancel);
-
+            var dateBtn = FindViewById<Button>(Resource.Id.btnHomeDate);
+            var selectedDate = FindViewById<EditText>(Resource.Id.homeDate);
+            var timeBtn = FindViewById<Button>(Resource.Id.btnHomeTime);
+            var selectedTime = FindViewById<EditText>(Resource.Id.homeTime);
+            dateBtn.Click += (o, e) => {
+                DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime time)
+                {
+                    selectedDate.Text = time.ToLongDateString();
+                });
+                frag.Show(FragmentManager, DatePickerFragment.TAG);
+            };
+            timeBtn.Click += (o, e) => {
+                TimePickerFragment frag = TimePickerFragment.NewInstance(delegate (TimePickerDialog.TimeSetEventArgs time)
+                {
+                    selectedTime.Text = time.HourOfDay + ":" + time.Minute;
+                });
+                frag.Show(FragmentManager, TimePickerFragment.TAG);
+            };
             saveBtn.Click += (o, e) => {
                 Finish();
             };
---
+++ PortFolioStatus/TimePickerFragment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace PortFolioStatus
+{
+    public class TimePickerFragment : DialogFragment,
+                                  TimePickerDialog.IOnTimeSetListener
+    {
+        // TAG can be any string of your choice.
+        public static readonly string TAG = "X:" + typeof(TimePickerFragment).Name.ToUpper();
+
+        // Initialize this value to prevent NullReferenceExceptions.
+        Action<TimePickerDialog.TimeSetEventArgs> _dateSelectedHandler = delegate { };
+
+        public static TimePickerFragment NewInstance(Action<TimePickerDialog.TimeSetEventArgs> onTimeSelected)
+        {
+            TimePickerFragment frag = new TimePickerFragment();
+            frag._dateSelectedHandler = onTimeSelected;
+            return frag;
+        }
+
+        public override Dialog OnCreateDialog(Bundle savedInstanceState)
+        {
+            DateTime currently = DateTime.Now;
+            TimePickerDialog dialog = new TimePickerDialog(Activity,
+                this, currently.Hour, currently.Minute, true);
+            return dialog;
+        }
+
+        public void OnTimeSet(TimePicker tp, int hour, int minutes)
+        {
+            Log.Debug(TAG, "Called time changed");
+            _dateSelectedHandler(new TimePickerDialog.TimeSetEventArgs(hour, minutes));
+        }
+    }
+}
